dba1b4ca12beee916513e2e7a9ceeee827bbc442
chrisYoder <christophermyoder@gmail.com>
2019-10-08 17:56:31 +0000

    initial commit

---
+++ Unit1/Week3/week3.py
@@ -0,0 +1,17 @@
+# Conditions
+x = int(input('select a number: '))
+if x < 10:
+    print('smaller')
+elif x > 20:
+    print('bigger')
+else: 
+    print('normal')
+    
+print('finis')
+
+# Repeated Steps
+n=5
+while n > 0:
+    print(n)
+    n = n - 1 
+print('Blastoff')
